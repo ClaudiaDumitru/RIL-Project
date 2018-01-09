16ec9847c5060099050591e51803630eec93ee94
claudiadumitru <dumitru.claudia.a@gmail.com>
2018-01-09 23:00:49 +0200

    final

--- main.py
+++ main.py
@@ -17,11 +17,6 @@ class Services(object):
     def index(self):
         return open('services.html')
 
-class About(object):
-    @cherrypy.expose
-    def index(self):
-        return open('about.html')
-
 class Contact(object):
     @cherrypy.expose
     def index(self):
@@ -31,7 +26,6 @@ if __name__=='__main__':
     root = Index()
     root.products = Products()
     root.services = Services()
-    root.about = About()
     root.contact = Contact()
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -41,6 +35,5 @@ if __name__=='__main__':
         '/fonts': {'tools.staticdir.on': True, 'tools.staticdir.dir': os.path.join(current_dir, 'fonts')},
         '/js': {'tools.staticdir.on': True, 'tools.staticdir.dir': os.path.join(current_dir, 'js')}
         }
-    cherrypy.config.update(conf)
 
-    cherrypy.quickstart(root, '/')
+    cherrypy.quickstart(root, '/', config=conf)
